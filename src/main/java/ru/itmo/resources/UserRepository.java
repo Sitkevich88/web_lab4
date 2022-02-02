ff0490e3167e2380f24820e281adff397317e8d0
Valerii Sitkevich <70900359+Sitkevich88@users.noreply.github.com>
2022-02-02 16:53:03 +0300

    Delete src directory

--- src/main/java/ru/itmo/resources/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.itmo.resources;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itmo.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByLogin(String login);
-}
